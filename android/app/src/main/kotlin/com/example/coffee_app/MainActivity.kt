1d038f0398a912667705dafbc454846bf6fa6159
Parables <parables95@gmail.com>
2021-12-10 20:18:36 +0000

    set the system navigation bar to transparent works on Android version 30 up

--- android/app/src/main/kotlin/com/example/coffee_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/coffee_app/MainActivity.kt
@@ -3,4 +3,11 @@ package com.example.coffee_app
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // set the System Navigation to transparent
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
+    }
 }
